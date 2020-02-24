d4525b88176c75fc6f6cbc06631efaff7cebcf9b
sai wanza <q020385791@gmail.com>
2020-02-24 16:28:31 +0800

    加入專案檔案。

---
+++ DesignList.cs
@@ -0,0 +1,132 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static CreateInstance.DesignList;
+
+namespace CreateInstance
+{
+    public class DesignList
+    {
+        private readonly Form1 form;
+        public DesignList(Form1 form)
+        {
+            this.form = form;
+        }
+        public class Mapping : Attribute
+        {
+            public string Method;
+            public string FunctionName;
+            public Mapping(string _Method, string _FunctionName)
+            {
+                this.Method = _Method;
+                this.FunctionName = _FunctionName;
+            }
+
+        }
+
+
+        public string InvokeRestful(string FunctionName, string methodName, JObject data)
+        {
+            //Type t = Type.GetType("CreateInstance.DesignList");
+            var method = Type.GetType("CreateInstance.DesignList").GetMethods()
+                        .Where(mi => mi.GetCustomAttributes(true)
+                        .Any(attr => attr is Mapping
+                        && ((Mapping)attr).FunctionName == FunctionName && ((Mapping)attr).Method == methodName)).First();
+            object DymaticClass = new object();
+            var ParamterName = method.GetParameters();
+            object[] objectlist = new object[ParamterName.Count()];
+
+            var dataCount = data.Count;
+            int C = 0;
+
+            foreach (var item in ParamterName)
+            {
+                Console.WriteLine(item.Name);
+                var Parametertype = item.ParameterType.FullName;
+                Console.WriteLine(Parametertype.ToString());
+                var oType = item.ParameterType;
+
+                if (oType == typeof(string))
+                {
+                    objectlist[C] = (string)data[item.Name] as string;
+
+                }
+                else if (oType == typeof(int))
+                {
+                    objectlist[C] = (int)data[item.Name];
+                }
+                else if (oType == typeof(char[]))
+                {
+                    objectlist[C] = data[item.Name].ToObject<char[]>();
+                }
+                else if (oType == typeof(object))
+                {
+                    objectlist[C] = data[item.Name].ToObject<object>();
+                }
+                else
+                {
+                    Type ClassType = Type.GetType(item.ParameterType.FullName, true);
+                    object[] ovalues = new object[data[item.Name].Count()];
+                    List<string> values = new List<string>();
+                    int Count = 0;
+                    ////////////////////////////
+                    var methodinfo = ClassType.GetMethod(item.ParameterType.FullName);
+                    ParameterInfo[] paramvalue = new ParameterInfo[] { };
+                    foreach (var singlevalue in data[item.Name])
+                    {
+
+                        var Valuetype = paramvalue[Count].ParameterType;
+
+                        if (Valuetype == typeof(string))
+                        {
+                            ovalues[Count] = (string)data[item.Name] as string;
+                        }
+                        else if (Valuetype == typeof(int))
+                        {
+                            ovalues[Count] = (int)data[item.Name];
+                        }
+                        else if (Valuetype == typeof(char[]))
+                        {
+                            ovalues[Count] = data[item.Name].ToObject<char[]>();
+                        }
+                        else
+                        {
+                            ovalues[Count] = (string)singlevalue;
+                        }
+                       
+                        Count++;
+                    }
+
+                    objectlist[C] = Activator.CreateInstance(ClassType, ovalues);
+                }
+                C++;
+            }
+            return (string)method.Invoke(this, objectlist);
+        }
+
+        [Mapping("GET", "StrParamType")]
+        public string StrParamType(string param1, string param2)
+        {
+
+            return "ok";
+        }
+        [Mapping("GET", "DifferentParam")]
+        public string DifferentParam(string strParam, int intParam)
+        {
+
+
+            return "ok2";
+        }
+
+
+
+    }
+    public class FunctionList
+    {
+        
+    }
+}
---
+++ ServerCenter.cs
@@ -0,0 +1,121 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using  DL=CreateInstance.DesignList;
+
+namespace CreateInstance
+{
+
+    internal class ServerCenter
+    {
+        private readonly Form1 form;
+        public ServerCenter(Form1 form)
+        {
+            this.form = form;
+        }
+
+        public void Run()
+        {
+            HttpListener server = new HttpListener();
+            try
+            {
+                //finish
+                server = new HttpListener
+                {
+                    //監聽90port
+                    Prefixes = { "http://127.0.0.1:90/" },
+                };
+                server.Start();
+            }
+            catch (Exception exx)
+            {
+
+                MessageBox.Show(exx.Message);
+            }
+            while (true)
+            {
+                var context = server.GetContext();
+                HttpListenerRequest request;
+                request = context.Request;
+                Stream body = request.InputStream;
+                Encoding encoding = request.ContentEncoding;
+
+                StreamReader reader = new StreamReader(body, encoding);
+
+                HttpListenerResponse response = context.Response;
+                string JsonData = reader.ReadToEnd();
+                string mdethod = context.Request.HttpMethod;
+                string FunctionName = context.Request.Url.Segments[1].Replace("/", "");
+                try
+                {
+                    //有帶data
+                    JObject jsonObj = new JObject();
+                    //取得為Get還是Post
+
+                    var method = Type.GetType("CreateInstance.DesignList").GetMethods()
+                        .Where(mi => mi.GetCustomAttributes(true)
+                        .Any(attr => attr is DL.Mapping
+                        && ((DL.Mapping)attr).FunctionName == FunctionName && ((DL.Mapping)attr).Method == mdethod)).First();
+                    if (JsonData != "")
+                    {
+
+                        jsonObj = JObject.Parse(JsonData);
+
+                    }
+                    else
+                    {
+                        //restful參數 方法後的參數 Ex:OpenCom/0
+                        string strParams = context.Request.Url
+                                           .Segments
+                                           .Skip(2)
+                                           .Select(k => k.Replace("/", "")).FirstOrDefault();
+
+                        object PackageNameAndData = new { FunctionName = FunctionName, data = strParams };
+                        jsonObj = JObject.FromObject(PackageNameAndData);
+                    }
+
+
+                    string result = "";
+                    ThreadPool.QueueUserWorkItem((_) =>
+                    {
+                        DL FunctionFiles = new DL(form);
+                        result = FunctionFiles.InvokeRestful(FunctionName, mdethod, jsonObj);
+                        string retstr = JsonConvert.SerializeObject(result);
+                        Console.WriteLine("result : " + (string)result);
+                        StringBuilder builder = new StringBuilder((string)result);
+                        string something = builder.ToString();
+                        byte[] buffer = Encoding.UTF8.GetBytes(something);
+                        response.ContentLength64 = buffer.Length;
+                        response.ContentType = "text/html";
+                        response.Headers.Add("Access-Control-Allow-Origin", "*");
+                        response.Headers.Add("Access-Control-Allow-Methods", "POST, GET");
+                        response.Headers.Add("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token");
+                        response.StatusDescription = "OK";
+                        Stream st = response.OutputStream;
+                        st.Write(buffer, 0, buffer.Length);
+                        context.Response.Close();
+
+
+                    });
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.Message);
+                    MessageBox.Show(e.Message);
+                }
+
+
+
+            }
+
+
+        }
+    }
+}
